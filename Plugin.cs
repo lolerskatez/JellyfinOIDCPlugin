007aea34f2ba23cf1a4f4554654d2d521939eacd
Your Name <your.email@example.com>
2025-12-17 13:57:07 -0600

    Add redirect page for configuration
    - Jellyfin loads redirect page via IHasWebPages
    - Redirect page redirects to /api/oidc/config endpoint
    - Avoids all routing conflicts while using both systems

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,16 @@
 #nullable enable
 
 using System;
+using System.Collections.Generic;
 using JellyfinOIDCPlugin.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace JellyfinOIDCPlugin;
 
-public class Plugin : BasePlugin<PluginConfiguration>
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
         : base(applicationPaths, xmlSerializer)
@@ -21,4 +23,17 @@ public class Plugin : BasePlugin<PluginConfiguration>
     public override Guid Id => Guid.Parse("7f8b8d9e-1234-5678-90ab-cdef12345678");
 
     public static Plugin? Instance { get; private set; }
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return new[]
+        {
+            new PluginPageInfo
+            {
+                Name = "OIDC Configuration",
+                EmbeddedResourcePath = "JellyfinOIDCPlugin.web.config-redirect.html",
+                DisplayName = "OIDC Authentication"
+            }
+        };
+    }
 }
